f2f6c9e8d97061a0dec8d1f09004334b1446a7d1
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-03-01 20:11:36 -0300

    Completed calculator prototype

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,48 +1,35 @@
 import { Button, ButtonGroup, Container, Grid } from "@mui/material";
 import BackspaceIcon from "@mui/icons-material/Backspace";
+import Box from "@mui/system/Box/Box";
 
 export default function Calculator() {
   return (
-    <Container
-      maxWidth="xs"
-      className="border border-2 border-blue-700 text-center py-3 m-2"
-    >
-      <Grid>
-        <ButtonGroup size="large" aria-label="large button group">
-          <Button variant="outlined">AC</Button>
+    <div>
+      <Box m={5}>
+        <Container
+          maxWidth="xs"
+          className="bg-black w-32 rounded-2xl text-center py-3 m-2"
+        >
+          <Button variant="outlined">
+            AC
+          </Button>
           <Button variant="outlined">
             <BackspaceIcon />
           </Button>
           <Button variant="outlined">%</Button>
           <Button variant="outlined">/</Button>
-        </ButtonGroup>
-      </Grid>
-      <Grid>
-        <ButtonGroup size="large" aria-label="large button group">
           <Button variant="outlined">7</Button>
           <Button variant="outlined">8</Button>
           <Button variant="outlined">9</Button>
           <Button variant="outlined">x</Button>
-        </ButtonGroup>
-      </Grid>
-      <Grid>
-        <ButtonGroup size="large" aria-label="large button group">
           <Button variant="outlined">4</Button>
           <Button variant="outlined">5</Button>
           <Button variant="outlined">6</Button>
           <Button variant="outlined">-</Button>
-        </ButtonGroup>
-      </Grid>
-      <Grid>
-        <ButtonGroup size="large" aria-label="large button group">
           <Button variant="outlined">1</Button>
           <Button variant="outlined">2</Button>
           <Button variant="outlined">3</Button>
           <Button variant="outlined">+</Button>
-        </ButtonGroup>
-      </Grid>
-      <Grid>
-        <ButtonGroup size="large" aria-label="large button group">
           <Button variant="outlined" size="large">
             0
           </Button>
@@ -52,8 +39,8 @@ export default function Calculator() {
           <Button variant="contained" size="large">
             =
           </Button>
-        </ButtonGroup>
-      </Grid>
-    </Container>
+        </Container>
+      </Box>
+    </div>
   );
 }
